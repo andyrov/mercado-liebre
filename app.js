9aae2c76855fc90e4752870ad1380c63d99cae1b
andyrov <andyrovner4@gmail.com>
2022-06-08 01:11:07 -0300

    version final

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath) );
 
 app.listen(process.env.PORT || 5050 , function (){
     console.log("Servidor corriendo en el puerto 5050");
